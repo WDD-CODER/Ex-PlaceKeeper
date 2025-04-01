712040e6ad73c350181052f072cd0c9cab656360
Your Name <dan.weibren@gmail.com>
2025-04-01 18:25:40 +0300

    General order on the code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 'use strict';
 
-// onInit(){}
+var gClockIntervalId
 
+
+function onInit() {
+    startClockAndDate() 
+}
+function startClockAndDate() {
+    updateClock()
+    ShowDate()
+    clearInterval(gClockIntervalId)
+    gClockIntervalId = setInterval(updateClock, 1000)
+}
+
+function moveToPage(str){
+    window.location.href = (str === 'map') ? 'map.html' : 'user-pref.html' 
+}
--- js/userService.js
+++ js/userService.js
@@ -1,1 +1,9 @@
 'use strict';
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromLocalStorage(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+}
